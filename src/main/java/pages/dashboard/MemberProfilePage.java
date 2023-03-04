145b8fa8e66324659a413becfb7a3cd6b58406c3
ArturPoturyan123 <poturyan@gmail.com>
2023-03-04 22:24:41 +0400

    fixed class name of MemberProfilePage

---
+++ src/main/java/pages/dashboard/MemberProfilePage.java
@@ -0,0 +1,26 @@
+package pages.dashboard;
+
+import com.codeborne.selenide.ClickOptions;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import pages.BasePage;
+
+public class MemberProfilePage extends BasePage<MemberProfilePage> {
+    private SelenideElement revokeInviteButton = Selenide.element("p.MuiTypography-root.MuiTypography-body1.css-1gollgz");
+
+
+
+
+
+    public void clickOnRevokeInviteButton (){
+
+        revokeInviteButton.click(ClickOptions.usingJavaScript());
+
+    }
+
+
+    @Override
+    public String getUrl() {
+        return null;
+    }
+}
