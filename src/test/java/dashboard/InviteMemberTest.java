4412732bb125880402d96f93e7c6d45b978333e1
arturp <poturyan@gmail.com>
2023-03-07 12:57:12 +0400

    Revert "space example"
    This reverts commit 784688f5b1bdaebc40a497727db7b718d16cbc20.

--- src/test/java/dashboard/InviteMemberTest.java
+++ src/test/java/dashboard/InviteMemberTest.java
@@ -36,6 +36,5 @@ public class InviteMemberTest extends DashboardTestBase {
                 .clickInviteButton();
         memberManagementPage.inviteTeamMember.shouldBe(Condition.visible);
         assertThat(memberManagementPage.getRowCount()).isEqualTo(rowSize + 1);
-        
     }
 }
