504d0a5ca2650d2e1295e46a431a4808f9a28edd
arturp <poturyan@gmail.com>
2023-03-24 11:50:28 +0400

    test addedaddd

--- src/test/java/dashboard/CompanyProfileTest.java
+++ src/test/java/dashboard/CompanyProfileTest.java
@@ -1,62 +1,62 @@
-package dashboard;
-
-import base.DashboardTestBase;
-import io.qameta.allure.Epic;
-import org.testng.annotations.Test;
-import pages.dashboard.CompanyProfilePage;
-import utils.RandomUtils;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-
-@Epic("Regression Tests")
-public class CompanyProfileTest extends DashboardTestBase {
-
-
-    @Test(testName = "Verify Functionality after saving Company Name any random texts")
-    public void verifyFunctionalitySavingNewCompanyName() throws InterruptedException {
-        CompanyProfilePage companyProfilePage = new CompanyProfilePage();
-        companyProfilePage.open();
-        String randomText = RandomUtils.getString();
-        companyProfilePage.setRandomCompanyName(randomText);
-        companyProfilePage.zoomPage();
-        Thread.sleep(2000);
-        companyProfilePage.clickOnSaveButton(companyProfilePage.saveButton);
-        companyProfilePage.resetZoom();
-        String currentCompanyName = companyProfilePage.setRandomCompanyName(randomText);
-        String getCurrentCompanyName = companyProfilePage.getCurrentCompanyName();
-        assertThat(currentCompanyName).isEqualTo(getCurrentCompanyName);
-
-    }
-
-    @Test(testName = "Verify Functionality after saving Company Address any random texts")
-
-    public void verifyFunctionalitySavingNewCompanyAddress() throws InterruptedException {
-        CompanyProfilePage companyProfilePage = new CompanyProfilePage();
-        companyProfilePage.open();
-        String randomText = RandomUtils.getString();
-        companyProfilePage.setRandomCompanyAddress(randomText);
-        companyProfilePage.zoomPage();
-        companyProfilePage.clickOnSaveButton(companyProfilePage.saveButton);
-        companyProfilePage.resetZoom();
-        String currentCompanyAddress = companyProfilePage.setRandomCompanyAddress(randomText);
-        String getCurrentCompanyAddress = companyProfilePage.getCurrentCompanyAddress();
-        assertThat(currentCompanyAddress).isEqualTo(getCurrentCompanyAddress);
-
-    }
-
-    @Test(testName = "Verify Functionality after saving Company Address any random City")
-    public void verifyFunctionalitySavingNewCityName() throws InterruptedException {
-        CompanyProfilePage companyProfilePage = new CompanyProfilePage();
-        companyProfilePage.open();
-        companyProfilePage.setRandomCityName();
-        companyProfilePage.zoomPage();
-        companyProfilePage.clickOnSaveButton(companyProfilePage.saveButton);
-        companyProfilePage.resetZoom();
-        String newCityName = companyProfilePage.setRandomCityName();
-        String getCurrentCityName = companyProfilePage.getCurrentCityName();
-        assertThat(newCityName).isEqualTo(getCurrentCityName);
-
-
-    }
-}
+//package dashboard;
+//
+//import base.DashboardTestBase;
+//import io.qameta.allure.Epic;
+//import org.testng.annotations.Test;
+//import pages.dashboard.CompanyProfilePage;
+//import utils.RandomUtils;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//
+//
+//@Epic("Regression Tests")
+//public class CompanyProfileTest extends DashboardTestBase {
+//
+//
+//    @Test(testName = "Verify Functionality after saving Company Name any random texts")
+//    public void verifyFunctionalitySavingNewCompanyName() throws InterruptedException {
+//        CompanyProfilePage companyProfilePage = new CompanyProfilePage();
+//        companyProfilePage.open();
+//        String randomText = RandomUtils.getString();
+//        companyProfilePage.setRandomCompanyName(randomText);
+//        companyProfilePage.zoomPage();
+//        Thread.sleep(2000);
+//        companyProfilePage.clickOnSaveButton(companyProfilePage.saveButton);
+//        companyProfilePage.resetZoom();
+//        String currentCompanyName = companyProfilePage.setRandomCompanyName(randomText);
+//        String getCurrentCompanyName = companyProfilePage.getCurrentCompanyName();
+//        assertThat(currentCompanyName).isEqualTo(getCurrentCompanyName);
+//
+//    }
+//
+//    @Test(testName = "Verify Functionality after saving Company Address any random texts")
+//
+//    public void verifyFunctionalitySavingNewCompanyAddress() throws InterruptedException {
+//        CompanyProfilePage companyProfilePage = new CompanyProfilePage();
+//        companyProfilePage.open();
+//        String randomText = RandomUtils.getString();
+//        companyProfilePage.setRandomCompanyAddress(randomText);
+//        companyProfilePage.zoomPage();
+//        companyProfilePage.clickOnSaveButton(companyProfilePage.saveButton);
+//        companyProfilePage.resetZoom();
+//        String currentCompanyAddress = companyProfilePage.setRandomCompanyAddress(randomText);
+//        String getCurrentCompanyAddress = companyProfilePage.getCurrentCompanyAddress();
+//        assertThat(currentCompanyAddress).isEqualTo(getCurrentCompanyAddress);
+//
+//    }
+//
+//    @Test(testName = "Verify Functionality after saving Company Address any random City")
+//    public void verifyFunctionalitySavingNewCityName() throws InterruptedException {
+//        CompanyProfilePage companyProfilePage = new CompanyProfilePage();
+//        companyProfilePage.open();
+//        companyProfilePage.setRandomCityName();
+//        companyProfilePage.zoomPage();
+//        companyProfilePage.clickOnSaveButton(companyProfilePage.saveButton);
+//        companyProfilePage.resetZoom();
+//        String newCityName = companyProfilePage.setRandomCityName();
+//        String getCurrentCityName = companyProfilePage.getCurrentCityName();
+//        assertThat(newCityName).isEqualTo(getCurrentCityName);
+//
+//
+//    }
+//}
--- src/test/java/dashboard/CustomizationRewardTest.java
+++ src/test/java/dashboard/CustomizationRewardTest.java
@@ -1,50 +1,50 @@
-package dashboard;
-
-import base.DashboardTestBase;
-import io.qameta.allure.Epic;
-import org.testng.annotations.Test;
-import pages.dashboard.CustomizationRewardsPage;
-import utils.RandomUtils;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-@Epic("Regression Tests")
-
-public class CustomizationRewardTest extends DashboardTestBase {
-
-
-    @Test(testName = "Verify to all toggles enable and disabled functionality")
-    public void verifyAllRewardsTogglesEnableAndDisable() throws InterruptedException {
-        CustomizationRewardsPage customizationRewardsPage = new CustomizationRewardsPage();
-        customizationRewardsPage.open();
-        customizationRewardsPage.clickAllToggles();
-        customizationRewardsPage.zoomPage();
-        customizationRewardsPage.clickOnSaveButton(customizationRewardsPage.saveButton);
-        customizationRewardsPage.resetZoom();
-        Thread.sleep(5000);
-        int activitiesSize = customizationRewardsPage.getActivitiesCount();
-        int togglesSize = customizationRewardsPage.getAllToggles();
-        assertThat(togglesSize).isEqualTo(activitiesSize + 1);
-        Thread.sleep(2000);
-        customizationRewardsPage.clickAllToggles();
-        customizationRewardsPage.zoomPage();
-        customizationRewardsPage.clickOnSaveButton(customizationRewardsPage.saveButton);
-        customizationRewardsPage.resetZoom();
-        Thread.sleep(3000);
-        assertThat(togglesSize).isNotEqualTo(activitiesSize);
-    }
-
-    @Test(testName = "Verify set your Rewards Points Goal")
-    public void verifySetRewardsPointsGoal() throws InterruptedException {
-        CustomizationRewardsPage customizationRewardsPage = new CustomizationRewardsPage();
-        customizationRewardsPage.open();
-        int randomNumber = RandomUtils.getInt();
-        customizationRewardsPage.setRandomRewardPointGoal(randomNumber);
-        customizationRewardsPage.zoomPage();
-        customizationRewardsPage.clickOnSaveButton(customizationRewardsPage.saveButton);
-        customizationRewardsPage.resetZoom();
-        assertThat(randomNumber).isEqualTo(customizationRewardsPage.getPhonePoints());
-
-
-    }
-}
+//package dashboard;
+//
+//import base.DashboardTestBase;
+//import io.qameta.allure.Epic;
+//import org.testng.annotations.Test;
+//import pages.dashboard.CustomizationRewardsPage;
+//import utils.RandomUtils;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//
+//@Epic("Regression Tests")
+//
+//public class CustomizationRewardTest extends DashboardTestBase {
+//
+//
+//    @Test(testName = "Verify to all toggles enable and disabled functionality")
+//    public void verifyAllRewardsTogglesEnableAndDisable() throws InterruptedException {
+//        CustomizationRewardsPage customizationRewardsPage = new CustomizationRewardsPage();
+//        customizationRewardsPage.open();
+//        customizationRewardsPage.clickAllToggles();
+//        customizationRewardsPage.zoomPage();
+//        customizationRewardsPage.clickOnSaveButton(customizationRewardsPage.saveButton);
+//        customizationRewardsPage.resetZoom();
+//        Thread.sleep(5000);
+//        int activitiesSize = customizationRewardsPage.getActivitiesCount();
+//        int togglesSize = customizationRewardsPage.getAllToggles();
+//        assertThat(togglesSize).isEqualTo(activitiesSize + 1);
+//        Thread.sleep(2000);
+//        customizationRewardsPage.clickAllToggles();
+//        customizationRewardsPage.zoomPage();
+//        customizationRewardsPage.clickOnSaveButton(customizationRewardsPage.saveButton);
+//        customizationRewardsPage.resetZoom();
+//        Thread.sleep(3000);
+//        assertThat(togglesSize).isNotEqualTo(activitiesSize);
+//    }
+//
+//    @Test(testName = "Verify set your Rewards Points Goal")
+//    public void verifySetRewardsPointsGoal() throws InterruptedException {
+//        CustomizationRewardsPage customizationRewardsPage = new CustomizationRewardsPage();
+//        customizationRewardsPage.open();
+//        int randomNumber = RandomUtils.getInt();
+//        customizationRewardsPage.setRandomRewardPointGoal(randomNumber);
+//        customizationRewardsPage.zoomPage();
+//        customizationRewardsPage.clickOnSaveButton(customizationRewardsPage.saveButton);
+//        customizationRewardsPage.resetZoom();
+//        assertThat(randomNumber).isEqualTo(customizationRewardsPage.getPhonePoints());
+//
+//
+//    }
+//}
