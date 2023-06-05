5bdc55fff3af4bd0c9bfa16606b6cfd48f35bb60
arturp <poturyan@gmail.com>
2023-06-06 01:25:42 +0400

    fix tests

--- src/test/java/dashboard/CreateCampaignTest.java
+++ src/test/java/dashboard/CreateCampaignTest.java
@@ -1,23 +1,25 @@
 package dashboard;
 
 import base.DashboardTestBase;
+import com.codeborne.selenide.Condition;
+import helper.WaitHelper;
 import io.qameta.allure.Epic;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.dashboard.CampaignsMainPage;
 import pages.dashboard.CreateCampaignPage;
 
+import java.time.Duration;
+
 import static pages.BasePage.isElementDisplayed;
 
 public class CreateCampaignTest extends DashboardTestBase {
-    CampaignsMainPage campaignsMainPage;
 
 
     @Epic("Regression Tests")
-
     @Test(testName = "Verify the functionality of the shows Top Bar on the Create Campaign page")
     public void verifyFunctionalityOfTheAppearTopBarInTheCreateCampaignFlow() {
-        campaignsMainPage = new CampaignsMainPage();
+        CampaignsMainPage campaignsMainPage = new CampaignsMainPage();
         CreateCampaignPage createCampaignPage = new CreateCampaignPage();
         campaignsMainPage.open();
         campaignsMainPage.clickCreateNewCampaignButton();
@@ -30,10 +32,12 @@ public class CreateCampaignTest extends DashboardTestBase {
     }
 
     @Test(testName = "Verify the functionality of return to campaign list ")
-    public void verifyFunctionalityOpensCampaignPage() {
+    public void verifyFunctionalityOfReturnToCampaignList() {
         CreateCampaignPage createCampaignPage = new CreateCampaignPage();
-        campaignsMainPage = new CampaignsMainPage();
+        CampaignsMainPage campaignsMainPage = new CampaignsMainPage();
         createCampaignPage.open();
+        WaitHelper.waitElementToPresent(createCampaignPage.createACampaignTitle,
+                Condition.visible, Duration.ofSeconds(5));
         createCampaignPage.clickGoBackButton();
         Assert.assertTrue(isElementDisplayed(campaignsMainPage.createNewCampaign),
                 "Error: Campaigns page is not present ");
